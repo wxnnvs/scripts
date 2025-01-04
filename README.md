c25794fe3b3605762eedd533cbfe5a22dc40c6a1
wxnnvs <136503688+wxnnvs@users.noreply.github.com>
2025-01-04 23:05:27 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Scripts
+These are personal setup scripts
+No support will be provided
---
+++ upload.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+if [ "$1" == "--remove" ]; then
+    rm -rf upload
+    echo "Removed 'upload' folder."
+else
+    # Check if the 'upload' folder exists
+    if [ ! -d "upload" ]; then
+        mkdir upload
+        cd upload
+    else
+        cd upload
+    fi
+
+    # Download the files
+    echo "Downloading files..."
+    curl -O https://raw.githubusercontent.com/wxnnvs/upload/refs/heads/dev/app.js > /dev/null 2>&1
+    curl -O https://raw.githubusercontent.com/wxnnvs/upload/refs/heads/dev/package.json > /dev/null 2>&1
+    curl -O https://raw.githubusercontent.com/wxnnvs/upload/refs/heads/dev/package-lock.json > /dev/null 2>&1
+
+    # Install the dependencies
+    echo "Installing dependencies..."
+    npm install > /dev/null 2>&1
+
+    # Run the application
+    while true; do
+        node app.js
+        echo "Restarting in 5 seconds..."
+        sleep 5
+    done
+fi
